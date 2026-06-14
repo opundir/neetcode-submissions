e64b277bd05e02b22d68c7cfba58ce649b1b5231
Ojasvi Pundir <84563117+opundir@users.noreply.github.com>
2026-06-14 23:49:04 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(int mid, int[] piles, int h){
+        int sum = 0;
+        for(int i=0; i<piles.length; i++){
+            double temp = (double)piles[i]/(double)mid;
+            sum += Math.ceil(temp);
+            // sum += (piles[i] + mid - 1) / mid;
+        }
+        return sum<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = 0;
+        for(int i : piles){
+            r = Math.max(i,r);
+        }
+        int ans = -1;
+        int mid;
+        while(l<r){
+            mid = l+(r-l)/2;
+            if(isValid(mid,piles,h)){
+                ans = mid;
+                r = mid;
+            }
+            else{
+                 l = mid+1;
+            }
+        }
+        return l;
+    }
+}
